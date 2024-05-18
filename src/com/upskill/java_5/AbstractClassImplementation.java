9f6801522cfccb6eddf99f7d6c4024d872a92726
EsmritaG <egurung12@hotmail.co.uk>
2024-05-18 14:04:00 -0400

    branching1

--- src/com/upskill/java_5/AbstractClassImplementation.java
+++ src/com/upskill/java_5/AbstractClassImplementation.java
@@ -1,23 +1,23 @@
 package com.upskill.java_5;
 
-public class AbstractClassImplementation extends AbstractClass {
+//public class AbstractClassImplementation extends AbstractClass {
 
-	@Override
-	public void iDoor() {
+	//@Override
+	//public void iDoor() {
 		// TODO Auto-generated method stub
-		System.out.println("My Car has 4 doors");
-	}
+	//	System.out.println("My Car has 4 doors");
+	//}
 
-	@Override
-	public void iWheel() {
-		System.out.println("My car has 4 wheels");// TODO Auto-generated method stub
-		return 4;
-	}
+	//@Override
+	//public void iWheel() {
+		//System.out.println("My car has 4 wheels");// TODO Auto-generated method stub
+	//	return 4;
+	//}
 
-	@Override
-	public void iSeat() {
-		System.out.println("My car has 4 seats");// TODO Auto-generated method stub
-		return 4;
-	}
+	//@Override
+	//public void iSeat() {
+		//System.out.println("My car has 4 seats");// TODO Auto-generated method stub
+	//	return 4;
+	//}
 
-}
+//}
